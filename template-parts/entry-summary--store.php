6620149cefec40162656677b5791c5eb8902fc51
hiroghap1 <hasegawayoshihiro@hasegawayoshihiro-no-MacBook-Pro.local>
2018-07-15 16:23:59 +0900

    refine-btn

--- template-parts/entry-summary--store.php
+++ template-parts/entry-summary--store.php
@@ -11,9 +11,10 @@ $seating = get_field('seating') == false ? 0 : get_field('seating');
 $open = get_field('open');
 $lo = get_field('last_order');
 $price = get_field('price');
+$address = get_field('address');
 ?>
 
-<li class="_c-entries__item _c-entries__item-store" data-open="<?php echo $open; ?>" data-lo="<?php echo $lo; ?>">
+<li class="_c-entries__item _c-entries__item-store" data-open="<?php echo $open; ?>" data-lo="<?php echo $lo; ?>" data-address="<?php echo mb_substr($address, 0, mb_strpos($address, '区')+1); ?>">
   <section class="_c-entry-summary _c-entry-summary-store">
     <header class="_c-entry-summary__header">
 		  <h2 class="_c-entry-summary__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -25,7 +26,7 @@ $price = get_field('price');
         </figure>
       </div>
       <div class="_c-row__col--3-4 _c-entry-summary__info-desc">
-        <p><i class="fa fa-map-marker" aria-hidden="true"></i> 大阪市<?php the_field('address'); ?></p>
+        <p><i class="fa fa-map-marker" aria-hidden="true"></i> 大阪市<?php echo $address; ?></p>
         <p class="store__tel"><a href="tel:<?php str_replace("-", "", the_field('tel')) ?>"><?php the_field('tel');?></a></p>
         <p><i class="fa fa-clock-o" aria-hidden="true"></i> <span class="store-open"><?php echo substr_replace($open, ':', 2, 0); ?></span>～<span class="store-lo"><?php echo substr_replace($lo, ':', 2, 0); ?>(L.O.)</span></p>
         <p>着席<?php echo $seating; ?>名 / 立食<?php echo $standing;?>名 / 合計<?php echo $seating + $standing; ?>名</p>
--- templates/view/archive-store.php
+++ templates/view/archive-store.php
@@ -7,7 +7,10 @@
 ?>
 
 <div class="store-archive_panel">
-  <p>並べ替え</p>
+  <div class="_c-row">
+  <button class="_c-btn _c-btn--block _u-text-left btn-sort_box" id="btn-sort_box">並べ替え</button>
+  <button class="_c-btn _c-btn--block _u-text-left btn-refine_box" id="btn-refine_box">絞り込み</button></div>
+  <div class="_c-box--sort_btn" id="_c-box--sort_btn">
   <button class="_c-btn _c-btn-sort" data-sort="open"><span>イベント開始時間</span>
     <span>
       <i class="fa fa-sort" aria-hidden="true"></i>
@@ -21,6 +24,15 @@
       <i class="fa fa-sort-desc" aria-hidden="true"></i>
     </span></button>
 </div>
+<div class="_c-box--refine_btn" id="_c-box--refine_btn">
+<label><input type="checkbox" name="address" value="北区" checked>北区</label>
+<label><input type="checkbox" name="address" value="都島区" checked>都島区</label>
+<label><input type="checkbox" name="address" value="福島区" checked>福島区</label>
+  <label><input type="checkbox" name="address" value="中央区" checked>中央区</label>
+  <label><input type="checkbox" name="address" value="浪速区" checked>浪速区</label>
+  <label><input type="checkbox" name="address" value="天王寺区" checked>天王寺区</label>
+  <label><input type="checkbox" name="address" value="東成区" checked>東成区</label>
+</div></div>
 <ul class="_c-entries" id="store-list">
 	<?php while (have_posts()) : the_post(); ?>
 		<?php get_template_part('template-parts/entry-summary--store'); ?>
